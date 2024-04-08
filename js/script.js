cfe2dfd8966dbe529d9fb7fc308a35a3026bd0ab
vmwebio <vmwebio@gmail.com>
2024-04-08 21:32:00 +0300

    add Choices custom select

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Choices js - Custom Select
+new Choices('#model', {
+    searchEnabled: false,
+    itemSelectText: '',
+})
+
+new Choices('#color', {
+    searchEnabled: false,
+    itemSelectText: '',
+})
