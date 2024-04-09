67f682a5a400bc306f66fbf81a82f7a1ac1934a8
vmwebio <vmwebio@gmail.com>
2024-04-09 14:13:51 +0300

    add jquery ajax form submit data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Choices js - Custom Select
+// Custom Select - Choices js
 new Choices('#model', {
     searchEnabled: false,
     itemSelectText: '',
@@ -8,3 +8,23 @@ new Choices('#color', {
     searchEnabled: false,
     itemSelectText: '',
 });
+
+// Form Order - Jquery
+$('#form-order').on('submit', function (event) {
+    event.preventDefault();
+
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        method: '',
+        dataType: 'html',
+        data: $('#form-order').serialize(),
+        success: function(data) {
+            console.log(data);
+            $('#form-order').html(`<div class="order__success_info"><h4>Ваш заказ отправлен!</h4></div>`);
+            $('.order__submit').hide();
+            $('#order').hide();
+            $('.order__fields_info').hide();
+        }
+    })
+});
+
